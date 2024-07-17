505c10bd9dff39fbfb8a7370d8f68b0fc13eb6de
Nyasha Rai <122803521+Nyasharai@users.noreply.github.com>
2024-07-17 23:44:34 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (54.14%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,24 +1,3 @@
-// class Solution {
-//     public int climbStairs(int n) {
-//         if(n == 0){
-//             return 1;
-//         }
-//         if(n < 0){
-//             return 0;
-//         }
-        
-//         int dp[] = new int[n+1];
-//         dp[0] = 1;
-//         dp[1] = 1;
-//         for(int i = 2; i <= n; i++){
-//             dp[i] = climbStairs(i-1) + climbStairs(i-2);
-//         }
-       
-//         return dp[n];
-        
-//     }
-// }
-
 class Solution {
     public int climbStairs(int n) {
         if (n == 0 || n == 1) {
@@ -37,3 +16,4 @@ class Solution {
         return second; // The result for dp[n]
     }
 }
+
