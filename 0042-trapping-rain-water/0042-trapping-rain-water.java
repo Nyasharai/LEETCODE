410ccfa5371f9fc74423bdba5e92de243abaf180
Nyasha Rai <122803521+Nyasharai@users.noreply.github.com>
2024-07-27 18:37:19 +0530

    Time: 1 ms (69.22%), Space: 46 MB (61.65%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,27 +1,30 @@
 class Solution {
     public int trap(int[] height) {
-        
-        int ans = 0;
-
-        int leftMax = 0, rightMax = 0;
-
-        int left = 0, right = height.length - 1;
-
-        while(left < right) {
-
-            if(height[left] <= height[right]) {
-                if(height[left] > leftMax) leftMax = height[left];
-                else ans += (leftMax - height[left]);
-                left++;
-            }
-
-            else {
-                if(height[right] > rightMax) rightMax = height[right];
-                else ans += (rightMax - height[right]);
-                right--;
-            }
+        int n = height.length;
+        if(height == null || n == 0){
+            return 0;
         }
-        return ans;
+        
+        int leftMax[] = new int[n];
+        int rightMax[] = new int[n];
+        leftMax[0] = height[0];
+        rightMax[n-1] = height[n-1];
+        
+        for(int i = 1; i < n; i++){
+            leftMax[i] = Math.max(leftMax[i-1], height[i]);
+        }
+        
+        for(int j = n-2; j >= 0; j--){
+            rightMax[j] = Math.max(rightMax[j+1],height[j]);
+        }
+        
+        int trapped = 0;
+        for(int p = 0; p < n; p++){
+            int waterLevel = Math.min(leftMax[p], rightMax[p]);
+            trapped = trapped + waterLevel - height[p];
+        }
+        
+        return trapped;
 
     }
 }
