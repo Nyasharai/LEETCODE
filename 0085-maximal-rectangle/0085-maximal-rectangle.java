7e6dd070458da929b5caca964f32f8b67d9ef1c6
Nyasha Rai <122803521+Nyasharai@users.noreply.github.com>
2024-07-29 20:08:12 +0530

    Time: 15 ms (64.91%), Space: 46.9 MB (26.03%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.java
+++ 0085-maximal-rectangle/0085-maximal-rectangle.java
@@ -1,33 +1,34 @@
 class Solution {
-    private int getMaximumArea(int[] height) {
-        Stack<Integer> stck = new Stack<>();
-        int area = 0;
-        for (int i = 0; i <= height.length; i++) {
-            while (!stck.isEmpty() && (i == height.length || height[stck.peek()] >= height[i])) {
-                int newHeight = height[stck.pop()];
-                int width = stck.isEmpty() ? i : i - stck.peek() - 1;
-                area = Math.max(area, width * newHeight);
+    public int getMaxArea(int[] heights){
+        int maxArea = 0;
+        Stack<Integer> stack = new Stack<>();
+        
+        int len = heights.length;
+        for(int i = 0 ; i <= len; i++){
+            int h = (i == len ? 0 : heights[i]);
+            while(!stack.isEmpty() && h < heights[stack.peek()]){
+                int height = heights[stack.pop()];
+                int width = stack.isEmpty() ? i : i - stack.peek() - 1;
+                maxArea = Math.max(height * width, maxArea);
             }
-            stck.push(i);
+            stack.push(i);
         }
-        return area;
+        return maxArea;
     }
-
     public int maximalRectangle(char[][] matrix) {
-        int area = 0;
-        int[] height = new int[matrix[0].length];
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
-                if (matrix[i][j] == '0') {
-                    height[j] = 0;
-                } else if (matrix[i][j] == '1') {
-                    height[j]++;
-                }
+        if (matrix.length == 0 || matrix[0].length == 0) return 0;
+        int cols = matrix[0].length;
+        int heights[] = new int[cols];
+        int maxArea = 0;
+        
+        for(char[] rows : matrix){
+            for(int j = 0; j < cols; j++){
+                heights[j] = rows[j] == '1' ? heights[j] + 1 : 0;
             }
-
-            int currentMaximumArea = getMaximumArea(height);
-            area = Math.max(area, currentMaximumArea);
+            maxArea = Math.max(maxArea, getMaxArea(heights));
         }
-        return area;
+        return maxArea;
     }
 }
+
+ 
