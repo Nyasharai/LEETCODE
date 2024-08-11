1561c8c64f46f96f31b41abce6bbb75ddeccf95a
Nyasha Rai <122803521+Nyasharai@users.noreply.github.com>
2024-08-11 19:07:00 +0530

    Time: 19 ms (55.84%), Space: 57.4 MB (66.99%) - LeetHub

--- 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.java
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.java
@@ -3,19 +3,39 @@ class Solution {
     // Taken some help
     
     public int furthestBuilding(int[] arr, int b, int l) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        int n = arr.length;
-        for(int i=0; i<n-1; i++){
-            int diff = arr[i+1]-arr[i];
+//         PriorityQueue<Integer> pq = new PriorityQueue<>();
+//         int n = arr.length;
+//         for(int i=0; i<n-1; i++){
+//             int diff = arr[i+1]-arr[i];
 
-            if(diff>0){
-                pq.add(diff);
+//             if(diff>0){
+//                 pq.add(diff);
+//             }
+//             if(pq.size()>l){
+//                 b = b-pq.remove();
+//             }
+//             if(b<0) return i;
+//         }
+//         return n-1;
+        
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        for(int i = 0; i < arr.length - 1; i++){
+            int diff = arr[i+1] - arr[i];
+            
+            if(diff > 0){
+                minHeap.offer(diff);
+                
+                if(minHeap.size() > l){
+                    int smallDiff = minHeap.poll();
+                    b -= smallDiff;
+                    
+                    if(b < 0){
+                        return i;
+                    }
+                }
             }
-            if(pq.size()>l){
-                b = b-pq.remove();
-            }
-            if(b<0) return i;
+            
         }
-        return n-1;
+        return arr.length - 1;
     }
 }
