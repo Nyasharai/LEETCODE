4f702fb52219b6af6939036fae793710eb183e60
Nyasha Rai <122803521+Nyasharai@users.noreply.github.com>
2024-09-30 21:49:54 +0530

    Time: 5 ms (93.47%), Space: 45.4 MB (72.25%) - LeetHub

---
+++ 1381-design-a-stack-with-increment-operation/1381-design-a-stack-with-increment-operation.java
@@ -0,0 +1,31 @@
+class CustomStack {
+    private int[] stack;  
+    private int maxSize;  
+    private int top;
+
+    public CustomStack(int maxSize) {
+        this.maxSize = maxSize;
+        this.stack = new int[maxSize];  
+        this.top = -1;
+    }
+    
+    public void push(int x) {
+        if (top < maxSize - 1) {
+            stack[++top] = x;  
+        }
+    }
+    
+    public int pop() {
+        if (top == -1) {
+            return -1;  
+        }
+        return stack[top--];
+    }
+    
+    public void increment(int k, int val) {
+        int limit = Math.min(k, top + 1);
+        for (int i = 0; i < limit; i++) {
+            stack[i] += val;
+        }
+    }
+}
